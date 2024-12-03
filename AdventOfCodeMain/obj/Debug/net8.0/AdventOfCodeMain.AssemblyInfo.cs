a9a97ccf68e8e3db31c96e45fae20f786354915f
Joel Schneider <jschneider2@online.gibz.ch>
2024-12-03 12:36:14 +0100

    Day 3 Part 2

--- AdventOfCodeMain/obj/Debug/net8.0/AdventOfCodeMain.AssemblyInfo.cs
+++ AdventOfCodeMain/obj/Debug/net8.0/AdventOfCodeMain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCodeMain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7317c2bff26d7ad498793b8340f216205537d85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+257093abccb062caaac21aa5588ad082bd5f429e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCodeMain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCodeMain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
