257093abccb062caaac21aa5588ad082bd5f429e
Joel Schneider <jschneider2@online.gibz.ch>
2024-12-03 08:21:00 +0100

    Day 3 Part 1

---
+++ AdventOfCodeMain/DaySolutions/Day03.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCodeMain.DaySolutions;
+
+internal class Day03
+{
+    static public void Run()
+    {
+        string challengeInput = FileReader.ReadFromFile("Day3.1");
+        string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";  
+        Regex regex = new Regex(pattern);
+        int totalSum = 0;
+
+        MatchCollection matches = regex.Matches(challengeInput);
+
+        foreach (Match match in matches)
+        {
+            int num1 = int.Parse(match.Groups[1].Value);
+            int num2 = int.Parse(match.Groups[2].Value);
+
+            totalSum += num1 * num2;
+        }
+        Console.WriteLine($"Total Sum of Products: {totalSum}");
+    }
+}
--- AdventOfCodeMain/obj/Release/net8.0/AdventOfCodeMain.AssemblyInfo.cs
+++ AdventOfCodeMain/obj/Release/net8.0/AdventOfCodeMain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCodeMain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d91de9c55ff59002e1b7ce9ea94c5a6005b421e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7317c2bff26d7ad498793b8340f216205537d85")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCodeMain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCodeMain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
