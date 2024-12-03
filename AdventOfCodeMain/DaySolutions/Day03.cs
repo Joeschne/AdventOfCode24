d7d31aef7230069f59040379b52da29dc0afff74
Joel Schneider <joelcschneider@gmx.ch>
2024-12-03 12:36:14 +0100

    Day 3 Part 2

--- AdventOfCodeMain/DaySolutions/Day03.cs
+++ AdventOfCodeMain/DaySolutions/Day03.cs
@@ -7,11 +7,19 @@ internal class Day03
     static public void Run()
     {
         string challengeInput = FileReader.ReadFromFile("Day3.1");
-        string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";  
-        Regex regex = new Regex(pattern);
+
+        Console.WriteLine($"Total sum: {CheckPart1(challengeInput)}");
+        Console.WriteLine($"Total sum conditional: {CheckPart2(challengeInput)}");
+
+    }
+    static private int CheckPart1(string challengeInput)
+    {
+        string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+        Regex multiplicationRegex = new Regex(pattern);
+
         int totalSum = 0;
 
-        MatchCollection matches = regex.Matches(challengeInput);
+        MatchCollection matches = multiplicationRegex.Matches(challengeInput);
 
         foreach (Match match in matches)
         {
@@ -20,6 +28,16 @@ internal class Day03
 
             totalSum += num1 * num2;
         }
-        Console.WriteLine($"Total Sum of Products: {totalSum}");
+        return totalSum;
+    }
+    static private int CheckPart2(string challengeInput)
+    {
+        int totalSum = 0;
+
+        string conditionalsPattern = @"don't\(\).*?(do\(\)|$)";
+
+        string result = Regex.Replace(challengeInput, conditionalsPattern, "", RegexOptions.Singleline);
+
+        return CheckPart1(result);
     }
 }
