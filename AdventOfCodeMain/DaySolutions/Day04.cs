4a8bc62e15e47f6e992b6685569fe943d5fde15d
Joel Schneider <joelcschneider@gmx.ch>
2024-12-04 18:35:50 +0100

    Day 04 Part 1

--- AdventOfCodeMain/DaySolutions/Day04.cs
+++ AdventOfCodeMain/DaySolutions/Day04.cs
@@ -1,67 +1,85 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
 
-namespace AdventOfCodeMain.DaySolutions;
-
-internal class Day04
+namespace AdventOfCodeMain.DaySolutions
 {
-    public static void Run()
+    internal class Day04
     {
-        string challengeInput = FileReader.ReadFromFile("Day4.1");
-        string pattern = @"(?=(XMAS|SAMX))";
-
-        string[] horizontalSlices = challengeInput.Split("\r\n");
-
-        int matchCounter = 0;
-
-        int rowLength = horizontalSlices[0].Length - 1;
-        int columnLength = horizontalSlices.Length - 1;
-
-        foreach (string horizontalSlice in horizontalSlices)
+        public static void Run()
         {
-            matchCounter += Regex.Matches(horizontalSlice, pattern).Count;
-        }
+            string challengeInput = FileReader.ReadFromFile("Day4.1");
+            string[] lines = challengeInput.Split("\r\n");
 
-        for (int column = 0; column <= rowLength; column++)
-        {
-            string verticalSlice = new string(horizontalSlices.Select(slice => slice[column]).ToArray());
-            matchCounter += Regex.Matches(verticalSlice, pattern).Count;
+            int numRows = lines.Length;
+            int numCols = lines[0].Length;
+            string word = "XMAS";
+            int matchCounter = 0;
 
-            string currentDiagonalRightSlice = "";
-            int columnIndex = column;
-            for (int row = 0; row <= columnLength; row++)
+
+            var stopwatch = new Stopwatch();
+
+            // Test flat loop
+            stopwatch.Start();
+            for (int pos = 0; pos < numRows * numCols; pos++)
             {
-                if (columnIndex <= rowLength)
+                int row = pos / numCols;
+                int col = pos % numCols;
+
+                if (lines[row][col] == word[0])
                 {
-                    currentDiagonalRightSlice += horizontalSlices[row][columnIndex];
-                    columnIndex++;
-                }
-                else
-                {
-                    matchCounter += Regex.Matches(currentDiagonalRightSlice, pattern).Count;
-                    currentDiagonalRightSlice = "";
-                    columnIndex = 0;
+                    foreach (var direction in directions)
+                    {
+                        if (IsWordInDirection(lines, row, col, word, direction, numRows, numCols))
+                        {
+                            matchCounter++;
+                        }
+                    }
                 }
             }
-            matchCounter += Regex.Matches(currentDiagonalRightSlice, pattern).Count;
-            string currentDiagonalLeftSlice = "";
-            columnIndex = column;
-            for (int row = 0; row <= columnLength; row++)
-            {
-                if (columnIndex >= 0)
-                {
-                    currentDiagonalLeftSlice += horizontalSlices[row][columnIndex];
-                    columnIndex--;
-                }
-                else
-                {
-                    matchCounter += Regex.Matches(currentDiagonalLeftSlice, pattern).Count;
-                    
-                    currentDiagonalLeftSlice = horizontalSlices[row][rowLength].ToString();
-                    columnIndex = rowLength - 1;
-                }
-            }
-            matchCounter += Regex.Matches(currentDiagonalLeftSlice, pattern).Count;
+
+            stopwatch.Stop();
+            Console.WriteLine($"Flat loop time: {stopwatch.ElapsedMilliseconds}ms"); // about 1-2ms
+
+            // Test LINQ version
+            stopwatch.Restart();
+            var matchCountLINQ = Enumerable.Range(0, numRows)
+                .SelectMany(row => Enumerable.Range(0, numCols), (row, col) => new { row, col })
+                .Where(pos => lines[pos.row][pos.col] == word[0])
+                .Sum(pos => directions.Count(direction =>
+                    IsWordInDirection(lines, pos.row, pos.col, word, direction, numRows, numCols)));
+
+            stopwatch.Stop();
+            Console.WriteLine($"LINQ time: {stopwatch.ElapsedMilliseconds}ms"); // about 3-8ms
+
+            Console.WriteLine($"Total matches found: {matchCounter}");
         }
-        Console.WriteLine(matchCounter);
+
+        private static readonly List<(int Row, int Col)> directions = new()
+        {
+            (-1,  0), // Up
+            ( 1,  0), // Down
+            ( 0, -1), // Left
+            ( 0,  1), // Right
+            (-1, -1), // Up-Left
+            (-1,  1), // Up-Right
+            ( 1, -1), // Down-Left
+            ( 1,  1)  // Down-Right
+        };
+
+        private static bool IsWordInDirection(string[] lines, int startRow, int startCol, string word, (int, int) direction, int numRows, int numCols)
+        {
+            for (int i = 1; i < word.Length; i++)
+            {
+                int newRow = startRow + i * direction.Item1;
+                int newCol = startCol + i * direction.Item2;
+
+                // Check boundaries
+                if (newRow < 0 || newRow >= numRows || newCol < 0 || newCol >= numCols)
+                    return false;
+
+                if (lines[newRow][newCol] != word[i])
+                    return false;
+            }
+            return true;
+        }
     }
 }
--- AdventOfCodeMain/obj/Debug/net8.0/AdventOfCodeMain.AssemblyInfo.cs
+++ AdventOfCodeMain/obj/Debug/net8.0/AdventOfCodeMain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCodeMain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a97ccf68e8e3db31c96e45fae20f786354915f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+257709741e024b7187fa874144645b295d2cf130")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCodeMain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCodeMain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
